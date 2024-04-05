5d79ac067ccc1def2027f9c72ec99c2a41b6ab0f
千代田桃 <chikorita157@ateliershiori.moe>
2024-04-05 16:25:51 -0400

    Data should be JSON

--- src/wordpress.py
+++ src/wordpress.py
@@ -16,7 +16,7 @@ def submit_text_post(title, body, tags):
 		info("Submitting post to WordPress")
 		newHeaders = {'Content-type': 'application/json'}
 		response = requests.post(_config.wordpress_url + '/wp-json/wp/v2/posts',
- 		data={'title': title, 'content' : body, 'tags' : tags, 'comment_status' : open, 'status' : 'publish'},
+ 		json={'title': title, 'content' : body, 'tags' : tags, 'comment_status' : open, 'status' : 'publish'},
  		headers=newHeaders, auth=(_config.wordpress_username, _config.wordpress_app_password))
 		print("Status code: ", response.status_code)
 		if response.status_code == 200:
@@ -37,7 +37,7 @@ def edit_text_post(postid, body):
 		print("Editing WordPress post: ", postid)
 		newHeaders = {'Content-type': 'application/json'}
 		response = requests.put(_config.wordpress_url + '/wp-json/wp/v2/posts/' + str(post_id) + '/',
- 		data={'content' : body},
+ 		json={'content' : body},
  		headers=newHeaders, auth=(_config.wordpress_username, _config.wordpress_app_password))
 		print("Status code: ", response.status_code)
 		if response.status_code == 200:
